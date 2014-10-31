b96dda6ac3ec4882cdb4f5c582a8e4dca20d1222
Gabriel Robitaille-Montpetit <grmontpetit@gmail.com>
2014-10-30 22:44:21 -0400

    Changed the copy script

--- scp_copy.sh
+++ scp_copy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp aj31080@10.196.113.189:/home/ens/AJ31080/Desktop/LAB01/* /export/LOG645/home/AJ31080/lab01
+scp user@server:/home/ens/AJ31080/Desktop/LAB01/* /export/LOG645/home/AJ31080/lab01
